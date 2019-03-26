7392c36352dd368e44b1cfb192ca718669f509b3
David Taylor <prooffreader@gmail.com>
2019-03-26 11:19:42 -0400

    Add return and input unique value validators

--- src/pysimpletabshellmenu/validators.py
+++ src/pysimpletabshellmenu/validators.py
@@ -112,5 +112,65 @@ def schema_is_valid(dict_):
             raise e
 
 
-def validate_input_overlaps():
-    pass
+def _find_tabs(dict_):
+    """Finds (or creates) 'tabs' list from config dict for following tests
+
+    :param dict_: config dict passed to Menu constructor
+    :type dict_: dict
+    :returns: list under 'tabs' key, explicit or implied
+    'rtype': list
+    """
+    schema_type = _determine_schema_type(dict_)
+    if schema_type == 'single_without_tabs':
+        return [dict_['items']] # wrapped in a list as if there were tabs specified
+    else:    
+        return dict_['tabs']
+
+
+def check_return_value_overlap(dict_):
+    """Validates that all return values in every tab are unique. In theory, this could be possible but since
+    multiple inputs are allowed for each menu item, in practice it is probably a mistake.
+    
+    :param dict_: config dict passed to Menu constructor
+    :type dict_: dict
+    :returns: None if valid config insofar as no return values overlap for any tab
+    :raises: AssertionError if return values overlap, giving tab (if multiple) and values
+    """
+    try:
+        tabs = _find_tabs(dict_)
+        for tab in tabs:
+            returns = [x['returns'] for x in tab['items']]
+            assert len(returns) == len(set(returns))
+    except AssertionError:
+        if 'header_choice_displayed_and_accepted' in tab.keys():
+            raise AssertionError(f'in tab {tab["header_choice_displayed_and_accepted"]}, there are repeated '
+                                 f'return values: {returns}')
+        else:
+            raise AssertionError(f'in the single tab, there are repeated return values: {returns}')
+
+
+def no_accepted_input_overlap(dict_):
+    """Validates that the potential inputs on each tab are unambiguous, i.e. that any entry will either lead
+    to another tab OR to returning a unique value OR the current tab's input value (this could have gone either
+    way, I chose not to accept duplicate tab name and input in that tab)
+    
+    :param dict_: config dict passed to Menu constructor
+    :type dict_: dict
+    :returns: None if valid config insofar as no input values overlap for any tab
+    :raises: AssertionError if input values overlap, giving tab (if multiple) and values
+    """
+    try:
+        tabs = _find_tabs(dict_)
+        tab_values = [x.get('header_choice_displayed_and_accepted', None) for x in tabs]
+        tab_values = [x for x in tabs if x is not None]
+        for tab in tabs:
+            input_values = tab_values[:]
+            for item in tab['items']:
+                input_values += item['valid_entries']
+                assert len(input_values) == len(set(input_values))
+    except AssertionError:
+        if 'header_choice_displayed_and_accepted' in tab.keys():
+            raise AssertionError(f'in tab {tab["header_choice_displayed_and_accepted"]}, there are repeated '
+                                 f'input values: {input values}, including other tabs')
+        else:
+            raise AssertionError(f'in the single tab, there are repeated input values: {input_values}')
