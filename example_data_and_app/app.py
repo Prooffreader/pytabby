987d22c7179f8a530a57f7e8e9cbcf3105bcd1ec
David Taylor <prooffreader@gmail.com>
2019-03-28 10:32:34 -0400

    Make app.py use both config files

--- example_data_and_app/app.py
+++ example_data_and_app/app.py
@@ -1,19 +1,22 @@
-"""A simple app to show a multiple tab example, it just echoes back the return values and keeps going forever."""
+"""A simple app that goes through the two example yaml config files, first the multiple tab, then the no-tab.
+The only action taken is the choices are printed to stdout, unless the 'quit' item is selected, in which case
+either the next config file starts or the program exits after both config files have been run"""
 
 import os
 import sys
 
 from pysimpletabshellmenu.menu import Menu
 
-CONFIG_FILE_NAME = "multiple_tabs_with_headers.yaml"  # in same directory as this script
+CONFIG_FILE_NAMES = ["multiple_tabs_with_headers.yaml",
+                     "one_tab_no_header.yaml"]
+# in same directory as this app.py script
 
-
-def main():
+def main(config_filename):
 
     this_dir = os.path.split(os.path.realpath(__file__))[0]
 
     # use staticmethod to read yaml
-    config = Menu.safe_read_yaml(os.path.join(this_dir, CONFIG_FILE_NAME))
+    config = Menu.safe_read_yaml(os.path.join(this_dir, config_filename))
 
     # instantiate
     menu = Menu(config)
@@ -27,8 +30,12 @@ def main():
 
         if value == "quit":
             print("\nQuitting!\n")
-            sys.exit(0)
+            return
 
 
 if __name__ == "__main__":
-    main()
+    for fn in CONFIG_FILE_NAMES:
+        print('\n##########')
+        print(f'# Using config file {fn}')
+        print('##########\n')
+        main(fn)
