13e30752f2b813f2fc2135eadf048946edca538d
David Taylor <prooffreader@gmail.com>
2019-04-01 09:58:13 -0400

    Add files and top boilerplate

---
+++ tests/test_tab.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# pylama:ignore=W293,W291,W391,E302,E128,E127,E303,E501 (will be fixed by black)
+
+from copy import deepcopy
+
+import pytest
+
+import tabbedshellmenus.tab as tab
+from tabbedshellmenus.tab import Tab
+
+pass
