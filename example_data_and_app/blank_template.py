82833571c770b504dd667f2f83f730527d16b930
David Taylor <prooffreader@gmail.com>
2019-04-13 07:53:11 -0400

    Updated templates and examples

--- example_data_and_app/blank_template.py
+++ example_data_and_app/blank_template.py
@@ -1,11 +1,11 @@
 # I personally don't like using .py files as config files, but I'm no purist, so here's an example.
-# note that this is not black-formated
+# note that this is not black-formated, but more compact in space
 
-config = {'case_sensitive': ,
-          'screen_width': ,
+config = {'case_sensitive': False,
+          'screen_width': 80,
           'tabs': [{'header_entry': '',
                     'header_description': '',
-                    'long_description': '',
+                    'header_long_description': '',
                     'items': [{'choice_displayed': '',
                                'choice_description': '',
                                'valid_entries': [''],
@@ -15,11 +15,11 @@ config = {'case_sensitive': ,
 
 # # above, but with every line commented with explanation
 #
-# config = {'case_sensitive': ,  # optional, boolean, default False
-#           'screen_width': ,  # optional, integer, default 80
-#           'tabs': [{'header_entry': '',  # required, will be changed to str if not already str
-#                     'header_description': '',  # optional, default None, will be changed to str if present, not None and not already str
-#                     'long_description': '',  # optional, default None, will be changed to str if present, not None and not already str
+# config = {'case_sensitive': False,  # optional, boolean, default False
+#           'screen_width': 80,  # optional, integer, default 80
+#           'tabs': [{'header_entry': '',  # required, the input entry required to select that tab
+#                     'header_description': '',  # can be missing or None
+#                     'header_long_description': '',  # can be missing or None
 #                     'items': [{'choice_displayed': '',  # required, will be changed to str if not already str
 #                                'choice_description': '',  # required, will be changed to str if not already str
 #                                'valid_entries': [''],  # list, can have multiple entries, each will be changed to str if not already str
