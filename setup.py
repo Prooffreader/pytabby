faadd467195a5fb6f8e45870a95cd6ea00227a99
David Taylor <prooffreader@gmail.com>
2019-04-25 18:41:30 -0400

    Ready for PyPI

--- setup.py
+++ setup.py
@@ -30,13 +30,14 @@ setup(
     name='pytabby',
     version='0.1.0',
     license='MIT',
-    description='A simple, nonopinionated python shell menu system WITH TABS',
+    description='A simple, non-opinionated python terminal menu system WITH TABS',
     long_description='%s' % (
         re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst'))
     ),
     author='David Taylor',
     author_email='prooffreader@gmail.com',
     url='http://github.com/Prooffreader/pytabby',
+    #download_url = 'https://github.com/Prooffreader/pytabby/archive/v010.tar.gz',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
@@ -45,17 +46,14 @@ setup(
     classifiers=[
         # complete classifier list:
         # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: Development Status :: 1 - Planning',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: Unix',
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
@@ -63,7 +61,7 @@ setup(
         'Topic :: Utilities',
     ],
     keywords=[
-        'python', 'shell', 'terminal', 'tabs', 'tabbed', 'menu', 'menus'
+        'python', 'shell', 'terminal', 'console', 'tabs', 'tabbed', 'menu', 'menus'
     ],
     install_requires=[
         'PyYAML>=5.1', 'schema>=0.7.0'
