2ce555d899e0957b0d4eaf0034ad0a768b9b1b2b
David Taylor <prooffreader@gmail.com>
2019-04-25 19:57:00 -0400

    moved conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,11 +45,11 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['build']
+exclude_patterns = ['build', '_build']
 
 source_suffix = '.rst'
 
-master_doc = 'index'
+#master_doc = 'index'
 
 language = None
 
