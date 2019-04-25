e804f2cbf2a660d8401d8a668ab35dce3eed8b3b
David Taylor <prooffreader@gmail.com>
2019-04-25 06:35:15 -0400

    Change, add and move example configs

---
+++ example_configs/blank_template.py
@@ -0,0 +1,11 @@
+config = {'case_sensitive': False,
+          'screen_width': 80,
+          'tabs': [{'tab_header_input': 'tabinput1',
+                    'tab_header_description': 'tabdescript1',
+                    'tab_header_long_description': 'tablongdescript2',
+                    'items': [{'item_choice_displayed': 'choice1',
+                               'item_description': 'descript1',
+                               'item_inputs': ['input1'],
+                               "item_returns": 'return1' }
+                    ]}]
+         }
