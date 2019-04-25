be8da50aae9d46e106304df66ba45a3217d75ca9
David Taylor <prooffreader@gmail.com>
2019-04-25 06:35:27 -0400

    Add version

--- src/tabbedshellmenus/__init__.py
+++ src/tabbedshellmenus/__init__.py
@@ -3,5 +3,9 @@
 
 """Just puts menu.Menu into the top tabbedshellmenus package namespace"""
 
+from . import _version
+
 from . import menu
 from .menu import Menu
+
+__version__ = _version.__version__
